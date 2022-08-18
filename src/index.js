774c4410f85837de83c7181aef2386287e2f7a0e
Dmitriy Panivnyk <d.panivnyk@ukr.net>
2022-08-18 16:53:13 +0300

    "fish" for project

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
 import './css/styles.css';
+import debounce from 'lodash.debounce';
+import Notiflix from 'notiflix';
+import { fetchCountries } from './fetchCountries.js';
 
 const DEBOUNCE_DELAY = 300;
+const searchBox = document.querySelector('#search-box');
+const countryList = document.querySelector('.country-list');
+const countryInfo = document.querySelector('.country-info');
